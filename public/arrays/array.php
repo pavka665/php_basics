8af2ffbbd47e1bfca55b93cbdd821252be8cdbfc
pavka <pavka665@gmail.com>
2024-08-05 22:44:28 +0200

    Working with arrays

---
+++ public/arrays/array.php
@@ -0,0 +1,43 @@
+<?php
+$output = '';
+$ids = [10, 87, 20, 45, 30, 67, 40, 50];
+$users = ['user1', 'user2', 'user3'];
+
+//  sort() sorts the origin array
+// sort($ids)
+
+// rsort() reverse sort 
+// rsort($ids)
+
+// array_push($ids, 100)
+// add to the end of the array new element
+
+// array_unshift($ids, 100) add new element at the begining of the array
+// array_unshift($ids, 21);
+
+// array_pop($ids) remove the last element on the array
+// array_pop($ids)
+
+// array_shift($ids) remove the fist element on the array
+// array_shift($ids);
+
+// array_slice($ids, start_index: 2, amount_elements: 4) slice array and create new array
+// $new_ids = array_slice($ids, 2, 4);
+
+// array_sum($ids) sum all elements of the array
+// $output = array_sum($ids);
+
+// array_search(20, $ids) sear for element in the array, return index 
+// $output = array_search(20, $ids);
+
+// in_array(20, $ids) return true or false if eleent is in the array
+// $output = in_array(32, $ids);
+
+// explode(',',  $arr) turn string to array 
+// $arr = 'tech.code.programming';
+// $output = explode('.', $arr);
+
+// implode(',', $arr) turn array into string
+// $output = implode(', ', $users);
+
+
